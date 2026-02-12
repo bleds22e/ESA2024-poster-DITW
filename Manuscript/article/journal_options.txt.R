0383d1e2097634d23d701862f9675e0559f35775
Ellen Bledsoe <ebledsoe@arizona.edu>
2026-02-12 15:55:43 -0700

    journal options

---
+++ Manuscript/article/journal_options.txt.R
@@ -0,0 +1,8 @@
+### Journal Options ###
+
+# Journal of Open Source Education (JOSE)
+# Journal of Statistics and Data Science Education
+# EcoSphere: Eco-Education
+# Ecology and Evolution
+# The American Biology Teacher
+# BioScience
